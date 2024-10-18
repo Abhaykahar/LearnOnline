e71f300b306b155aed488e07e0c28d07d484d9b4
Abhay kahar <ak2415112@gmail.com>
2024-10-19 02:06:19 +0530

    lerning

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CatalogPage from './pages/CatalogPage';
+import CourseDetailPage from './pages/CourseDetailPage';
+import DashboardPage from './pages/DashboardPage';
+import LessonPlayerPage from './pages/LessonPlayerPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header'
+import './app.css'
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <div className="app-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/course/:courseId" element={<CourseDetailPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/course/:courseId/lesson" element={<LessonPlayerPage />} />
+         
+        </Routes>
+      </div>
+      <ToastContainer />
+    </Router>
+  );
+}
+
+export default App;
