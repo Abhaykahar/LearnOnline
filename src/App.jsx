90a1340446288df09196f002bda5fe11e6506871
Abhay kahar <ak2415112@gmail.com>
2024-10-19 05:02:53 +0530

    lerning

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CourseDetailPage from './pages/CourseDetailPage';
 import DashboardPage from './pages/DashboardPage';
 import LessonPlayerPage from './pages/LessonPlayerPage';
 import { ToastContainer } from 'react-toastify';
-import './app.css'
+import './App.css'
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header'
 
